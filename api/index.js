40d65bc4e606b6c55b40bd54b32f220f07c1f170
duliangkuan <2330304961@qq.com>
2025-11-17 18:18:15 +0800

    Remove: All deployment scripts and Chinese documentation files

--- api/index.js
+++
@@ -1,8 +0,0 @@
-// Vercel Serverless Functions entry point
-// This file routes all API requests to the appropriate handler
-
-const app = require('../backend/src/server');
-
-// Export as Vercel serverless function
-module.exports = app;
-
